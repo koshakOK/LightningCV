26184d894040d8539a4f1d9ab8a44a363f8f4609
Maxim <Mkleshchev@sberbank.ru>
2022-02-01 10:15:23 +0000

    adds loss

--- Segmentation/loss.py
+++ Segmentation/loss.py
@@ -2,6 +2,16 @@ import torch
 
 
 def dice_loss(pred, target, smooth = 1.):
+    """[summary]
+
+    Args:
+        pred (torch.Tensor): predictions
+        target (torch.Tensor): ground thuth
+        smooth ([type], optional):  defaults to 1..
+
+    Returns:
+        torch.Tensor: mean loss throw batch
+    """
     pred = pred.contiguous()
     target = target.contiguous()    
     intersection = (pred * target).sum(dim=2).sum(dim=2)
