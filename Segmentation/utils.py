94d7c5639788d65f25f852306c97ae21702d8ce1
Maxim <Mkleshchev@sberbank.ru>
2022-02-01 10:38:02 +0000

    fiz

--- Segmentation/utils.py
+++ Segmentation/utils.py
@@ -6,6 +6,7 @@ NUM_CLASSES = 1
 MAX_EPOCHS = 50
 SMOOTH = 1e-6
 
+
 def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
     """Torch realization of iou metric.
         params:
@@ -13,12 +14,12 @@ def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
             labels shape: BATCH x H x W
         return: IoU score
     """
-    
+
     intersection = (outputs.long() & labels.long()).float().sum((1, 2))
     union = (outputs.long() | labels.long()).float().sum((1, 2))
-    
+
     iou = (intersection + SMOOTH) / (union + SMOOTH)
-    
+
     thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10
-    
+
     return thresholded.mean()
